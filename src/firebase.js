16e483a629db45509819e8ecdef836e86edf914c
Noel Arzola <noel@skillcrush.com>
2022-07-25 22:10:23 -0400

    Add nav, banner, rows

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLl8lkM1JHjN9HfodyM8Y7MmFJAwovOYI",
+  authDomain: "netflix-152b6.firebaseapp.com",
+  projectId: "netflix-152b6",
+  storageBucket: "netflix-152b6.appspot.com",
+  messagingSenderId: "628913962186",
+  appId: "1:628913962186:web:c09e16742397d435db4833",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
