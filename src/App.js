d9e33833f1513bdbe1d49215d3bb91d58f4b033f
Noel Arzola <noel@skillcrush.com>
2022-07-27 03:11:16 -0400

    Add Homescreen, Login. Start profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import Homescreen from "./Homescreen";
+import Homescreen from "./screens/Homescreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import Profile from "./screens/Profile";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
   return (
     <div className="App">
-      <Homescreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/">
+              <Homescreen />
+            </Route>
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
---
+++ src/screens/Profile.js
@@ -0,0 +1,39 @@
+import "./Profile.css";
+import Nav from "../Nav";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+
+function Profile() {
+  const user = useSelector(selectUser);
+  return (
+    <div className="profileScreen">
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+            alt=""
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+              <p>Renew Date</p>
+              <button
+                onClick={() => auth.signOut}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
