7ad180f4a22b5b11714f9321facd3f7f2976a6ae
Noel Arzola <noel@skillcrush.com>
2023-11-09 09:55:58 -0500

    bring email to the next screen on first load, add credentials warning

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -2,7 +2,8 @@ import React, { useRef } from "react";
 import { auth } from "../firebase";
 import "./SignUpScreen.css";
 
-function SignUpScreen() {
+function SignUpScreen({ getStartedEmailRef }) {
+  console.log(getStartedEmailRef.current);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
@@ -38,16 +39,26 @@ function SignUpScreen() {
     <div className="signUpScreen">
       <form action="">
         <h1>Sign In</h1>
-        <input ref={emailRef} type="email" placeholder="Email" />
+        <input
+          ref={emailRef}
+          type="email"
+          placeholder="Email"
+          defaultValue={
+            getStartedEmailRef.current.value
+              ? getStartedEmailRef.current.value
+              : ""
+          }
+        />
         <input ref={passwordRef} type="password" placeholder="Password" />
         <button type="submit" onClick={signIn}>
           Sign In
         </button>
         <h4>
-          <span>New to Netflix?</span>{" "}
+          <span>New to Netflix? Fill out the form above then</span>{" "}
           <span className="signUpScreen__link" onClick={register}>
-            Sign Up Now.
-          </span>
+            click here to Sign Up Now.
+          </span>{" "}
+          <span> (Use made up credentials!)</span>
         </h4>
       </form>
     </div>
